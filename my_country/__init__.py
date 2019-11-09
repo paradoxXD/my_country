892e268b505c07e4be8753128ca7794624df7055
Yaochen Zhu <118020081@link.cuhk.edu.cn>
2019-11-09 13:19:38 +0800

    Clean up the code
    use jinja to make the code more beautiful

--- my_country/__init__.py
+++ my_country/__init__.py
@@ -33,16 +33,22 @@ def create_app(test_config=None):
     @app.route('/icons')
     def icons():
         return render_template('icons.html')
-    
-    @app.route('/map')
-    def map():
-        return render_template('map.html')
 
     @app.route('/notifications')
     def notifications():
         return render_template('notifications.html')
 
-    
+    @app.route('/user')
+    def user():
+        return render_template('user.html')
+
+    @app.route('/tables')
+    def tables():
+        return render_template('tables.html')
+
+    @app.route('/typography')
+    def typography():
+        return render_template('typography.html')
 
     
 
